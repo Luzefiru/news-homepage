b252c17a69cdce08c32515672e376f77f6d02894
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-15 13:42:38 +0800

    feat: extract SideArticles as its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import MainArticle from './components/MainArticle';
+import SideArticles from './components/SideArticles';
 
 function App() {
   return (
@@ -8,43 +9,7 @@ function App() {
 
       <main className="grid grid-cols-3 gap-x-6 gap-y-12">
         <MainArticle />
-        <section className="bg-very-dark-blue p-7">
-          <h2 className="text-soft-orange text-4xl font-bold">New</h2>
-          <article>
-            <a href="/">
-              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
-                Hydrogen VS Electric Cars
-              </h3>
-            </a>
-            <p className="text-grayish-blue mb-7 leading-7">
-              Will hydrogen-fueled cars ever catch up to EVs?
-            </p>
-          </article>
-          <hr className="border-0 h-px bg-dark-grayish-blue" />
-          <article>
-            <a href="/">
-              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
-                The Downsides of AI Artistry
-              </h3>
-            </a>
-            <p className="text-grayish-blue mb-7 leading-7">
-              What are the possible adverse effects of on-demand AI image
-              generation?
-            </p>
-          </article>
-          <hr className="border-0 h-px bg-dark-grayish-blue" />
-          <article>
-            <a href="/">
-              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
-                Is VC Funding Drying Up?
-              </h3>
-            </a>
-            <p className="text-grayish-blue mb-7 leading-7">
-              Private funding by VC firms is down 50% YOY. We take a look at
-              what that means.
-            </p>
-          </article>
-        </section>
+        <SideArticles />
         <section className="col-span-full">
           <article>
             <div>01</div>
---
+++ src/components/SideArticles.jsx
@@ -0,0 +1,43 @@
+const SideArticles = () => {
+  return (
+    <section className="bg-very-dark-blue p-7">
+      <h2 className="text-soft-orange text-4xl font-bold">New</h2>
+      <article>
+        <a href="/">
+          <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+            Hydrogen VS Electric Cars
+          </h3>
+        </a>
+        <p className="text-grayish-blue mb-7 leading-7">
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </p>
+      </article>
+      <hr className="border-0 h-px bg-dark-grayish-blue" />
+      <article>
+        <a href="/">
+          <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+            The Downsides of AI Artistry
+          </h3>
+        </a>
+        <p className="text-grayish-blue mb-7 leading-7">
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </article>
+      <hr className="border-0 h-px bg-dark-grayish-blue" />
+      <article>
+        <a href="/">
+          <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+            Is VC Funding Drying Up?
+          </h3>
+        </a>
+        <p className="text-grayish-blue mb-7 leading-7">
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default SideArticles;
