bd35dc040698b5a365aacdd3d294a34555d1d37d
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-15 16:15:26 +0800

    fix: make z-index auto on larger screens

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App mb-14 lg:mx-[4rem] lg:mt-[3rem] lg:mb-[4rem] xl:mx-[10rem] xl:mt-[5rem] xl:mb-[8rem]">
       <Header />
-      <main className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-6 lg:gap-y-12 p-4 lg:p-0 absolute md:relative -z-10">
+      <main className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-6 lg:gap-y-12 p-4 lg:p-0 absolute md:relative -z-10 md:z-auto">
         <MainArticle />
         <NewArticles />
         <TrendingArticles />
