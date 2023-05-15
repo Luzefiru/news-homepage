f9876606fa804f3c94fcd654c492986b421d453a
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-15 13:41:44 +0800

    feat: finish side section

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,38 @@ function App() {
 
       <main className="grid grid-cols-3 gap-x-6 gap-y-12">
         <MainArticle />
-        <section>
-          <h2>New</h2>
+        <section className="bg-very-dark-blue p-7">
+          <h2 className="text-soft-orange text-4xl font-bold">New</h2>
           <article>
             <a href="/">
-              <h3>New Hydrogen VS Electric Cars</h3>
+              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+                Hydrogen VS Electric Cars
+              </h3>
             </a>
-            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+            <p className="text-grayish-blue mb-7 leading-7">
+              Will hydrogen-fueled cars ever catch up to EVs?
+            </p>
           </article>
-          <hr />
+          <hr className="border-0 h-px bg-dark-grayish-blue" />
           <article>
             <a href="/">
-              <h3>The Downsides of AI Artistry</h3>
+              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+                The Downsides of AI Artistry
+              </h3>
             </a>
-            <p>
+            <p className="text-grayish-blue mb-7 leading-7">
               What are the possible adverse effects of on-demand AI image
               generation?
             </p>
           </article>
-          <hr />
+          <hr className="border-0 h-px bg-dark-grayish-blue" />
           <article>
             <a href="/">
-              <h3>Is VC Funding Drying Up?</h3>
+              <h3 className="text-white font-bold text-xl mt-7 mb-2 hover:text-soft-orange">
+                Is VC Funding Drying Up?
+              </h3>
             </a>
-            <p>
+            <p className="text-grayish-blue mb-7 leading-7">
               Private funding by VC firms is down 50% YOY. We take a look at
               what that means.
             </p>
