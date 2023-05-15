a534287c9801294b2e0a02e9c69182ce1586d66f
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-15 13:05:03 +0800

    feat: finish header

--- src/App.js
+++ src/App.js
@@ -3,25 +3,50 @@ import Logo from './assets/images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <div className="App xl:m-[10rem] xl:mt-[5rem] xl:mb-[8rem]">
+      <header className="flex justify-between mb-12">
         <img src={Logo} alt="Site Logo" />
-        <nav>
-          <ul>
+        <nav className="flex items-center">
+          <ul className="flex">
             <li>
-              <a href="/">Home</a>
+              <a
+                className="px-4 py-2 font-medium text-dark-grayish-blue hover:text-soft-red"
+                href="/"
+              >
+                Home
+              </a>
             </li>
             <li>
-              <a href="/">New</a>
+              <a
+                className="px-4 py-2 font-medium text-dark-grayish-blue hover:text-soft-red"
+                href="/"
+              >
+                New
+              </a>
             </li>
             <li>
-              <a href="/">Popular</a>
+              <a
+                className="px-4 py-2 font-medium text-dark-grayish-blue hover:text-soft-red"
+                href="/"
+              >
+                Popular
+              </a>
             </li>
             <li>
-              <a href="/">Trending</a>
+              <a
+                className="px-4 py-2 font-medium text-dark-grayish-blue hover:text-soft-red"
+                href="/"
+              >
+                Trending
+              </a>
             </li>
             <li>
-              <a href="/">Categories</a>
+              <a
+                className="px-4 py-2 font-medium text-dark-grayish-blue hover:text-soft-red"
+                href="/"
+              >
+                Categories
+              </a>
             </li>
           </ul>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'soft-red': 'hsl(5, 85%, 63%)',
         'off-white': 'hsl(36, 100%, 99%)',
         'grayish-blue': 'hsl(233, 8%, 79%)',
-        'dark-greyish-blue': 'hsl(236, 13%, 42%)',
+        'dark-grayish-blue': 'hsl(236, 13%, 42%)',
         'very-dark-blue': 'hsl(240, 100%, 5%)',
       },
       fontFamily: {
