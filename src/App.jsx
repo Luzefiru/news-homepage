17ccc31620bb26ab60a4c8558d463ee1c2b2761d
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-15 13:24:21 +0800

    feat: finish main article section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import Header from './components/Header';
+import MainCover from './assets/images/image-web-3-desktop.jpg';
 
 function App() {
   return (
     <div className="App xl:m-[10rem] xl:mt-[5rem] xl:mb-[8rem]">
       <Header />
-      <main className="grid grid-cols-3 gap-6">
+      <main className="grid grid-cols-3 gap-x-6 gap-y-12">
         <article className="col-span-2">
-          <h1>The Bright Future of Web 3.0?</h1>{' '}
-          <div>
-            <p>
-              We dive into the next evolution of the web that claims to put the
-              power of the platforms back into the hands of the people. But is
-              it really fulfilling its promise?
-            </p>
-            <a href="/">Read More</a>
+          <img alt="Geometric Shapes" src={MainCover} />
+          <div className="grid grid-cols-2 mt-8 gap-4">
+            <h1 className="font-black text-6xl text-very-dark-blue">
+              The Bright Future of Web 3.0?
+            </h1>
+            <div className="relative">
+              <p className="text-dark-grayish-blue">
+                We dive into the next evolution of the web that claims to put
+                the power of the platforms back into the hands of the people.
+                But is it really fulfilling its promise?
+              </p>
+              <a
+                className="absolute bottom-0 uppercase tracking-[0.25rem] font-semibold bg-soft-red hover:bg-very-dark-blue py-3 px-9 text-white"
+                href="/"
+              >
+                Read More
+              </a>
+            </div>
           </div>
         </article>
         <section>
